0122ed93c8aae5c36953be502edd9ad0d5205347
Cong <21111062@gmail.com>
2025-01-09 16:03:32 +0700

    giao diện

---
+++ KhachSan/src/main/java/com/example/KhachSan/controller/OrderController.java
@@ -0,0 +1,42 @@
+package com.example.KhachSan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/views/order")
+public class OrderController {
+
+    @GetMapping("/pl")
+    public String index(Model model) {
+        model.addAttribute("pageTitle", "Sản phẩm");
+        return "place_order";
+    }
+
+
+    @GetMapping("/place-order/{code}")// link này chuyển trang trong layout dùng chung
+    public String shop(@PathVariable("code") String code, Model model) {
+        model.addAttribute("pageTitle", "Thanh toán đơn hàng");
+
+        return "place_order"; //  này là chỉ tới trang giao diên html
+    }
+
+    @GetMapping()
+    public String listOrder() {
+        return "order";
+    }
+
+    @GetMapping("/payment/result/{code}")
+    public String resultPayment(@PathVariable String code) {
+        return "place_order";
+    }
+
+    @GetMapping("/detail/{code}")
+    public String detailOredr(@PathVariable String code) {
+        return "order_detail";
+    }
+
+}
